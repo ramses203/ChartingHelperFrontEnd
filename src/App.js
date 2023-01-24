344171711df9767694b9b6988ce1f91594261dab
Johngreen <ramses203@naver.com>
2023-01-24 16:48:13 +0900

    test3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  // const buttonEvent = () => {
+  //   axios.post("http://localhost:3001/Depth1/test").then((res) => {
+  //     console.log("res : ", res);
+  //   });
+  // };
+
+  const buttonEvent = () => {
+    axios
+      .post(
+        "https://web-chartinghelperfrontend-sop272gld9krimc.gksl2.cloudtype.app/:3001/Depth1/test"
+      )
+      .then((res) => {
+        console.log("res : ", res);
+      });
+  };
+
   return (
     <>
       <h2>First Page</h2>
+      <button onClick={buttonEvent}>Click!</button>
       <select>test1</select>
       <select>test2</select>
       <select>test3</select>
