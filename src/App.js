b371a06bf8d84357dea4ca2c911a5f602ec19618
Johngreen <ramses203@naver.com>
2023-01-24 13:37:02 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>First Page</h2>
+      <select>test1</select>
+      <select>test2</select>
+      <select>test3</select>
+    </>
   );
 }
 
